4a4ea99d879378660d74526d8eedf20944922997
chutzpaheron <laura.villarreal@ucas-edu.net>
2026-09-09 14:51:48 -0600

    variable notes

---
+++ variable_notes.py
@@ -0,0 +1,11 @@
+# LV, variable notes, period 7
+# comments always start with the hash symbol and computer cannot read them. why is it autofilling stop it let me write it myself
+name = input("Give Me Your Name: ")  # create a variable, keyword [input], parentheses around input/action, ask a question>!?!??!?
+
+print(name)  # you can print more than one thing
+print("panda")
+print("caw caw")
+print("hello" ,name)
+# IT KEEPS AUTOFLLING IT TRIED TO SUGGEST LIAM GO AWAY
+# initialize by naming, assignment operator, declare by giving value, that is all 3 steps < INPUT
+# write PRINT, put parentheses, insert what to print in paretheses < OUTPUT
