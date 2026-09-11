457e6d93f2aa4fe16c6f0630644c2a664d542efa
chutzpaheron <laura.villarreal@ucas-edu.net>
2026-09-11 14:45:08 -0600

    logical_memory variable_practice

---
+++ logical_memory.py
@@ -0,0 +1,8 @@
+# LV, memory notes, period 7
+pi = 3.1415926
+
+school = "UCAS"
+
+print(f"the variable pi has a value of {pi} and is saved at {id(pi)}")
+
+print(f"the variable school has a value of {school} and is saved at {id(school)}")
---
+++ variable_practice.py
@@ -0,0 +1,14 @@
+# LV, variable practice, P7
+name = "NAME"
+
+number1 = "5"
+
+number2 = "123"
+
+breakfast = "bacon and eggs"
+
+color = "PURPLE!!!"
+
+school_name = "UCAS"
+
+year = "2026"
