8fea47a8cb542bfbc994f31bc895c29309c816fc
chutzpaheron <laura.villarreal@ucas-edu.net>
2026-09-11 14:51:58 -0600

    variable_practice

--- variable_practice.py
+++ variable_practice.py
@@ -1,14 +1,21 @@
 # LV, variable practice, P7
-name = "NAME"
-
+name = "robin"
+print(name)
 number1 = "5"
-
+print(number1)
 number2 = "123"
-
+print(number2)
 breakfast = "bacon and eggs"
-
-color = "PURPLE!!!"
-
+print(breakfast)
+color = "PURPLE"
+print(color)
 school_name = "UCAS"
-
+print(school_name)
 year = "2026"
+print(year)
+eye_color = "dark brown"
+print(eye_color)
+age = "15"
+print(age)
+favorite_subject = "biology"
+print(favorite_subject)
