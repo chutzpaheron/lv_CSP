ef43eb55b87e393aff06ef153528cdbe9cd3c204
chutzpaheron <laura.villarreal@ucas-edu.net>
2026-09-14 14:45:48 -0600

    memory_diagram

---
+++ memory_diagram.py
@@ -0,0 +1,25 @@
+name = "Alex"
+age = 14
+
+print(f"the variable name has a value of {name} and is saved at {id(name)}")
+print(f"the variable age has a value of {age} and is saved at {id(age)}")
+
+score = 10
+score = 25
+
+print(f"the variable score has a value of {score} and is saved at {id(score)}")
+print(f"the variable score has a value of {score} and is saved at {id(score)}")
+
+height = 5.9
+city = "Denver"
+zip_code = 80202
+
+print(f"the variable height has a value of {height} and is saved at {id(height)}")
+print(f"the variable city has a value of {city} and is saved at {id(city)}")
+print(f"the variable zip_code has a value of {zip_code} and is saved at {id(zip_code)}")
+
+price1 = 10
+price2 = 10
+
+print(f"the variable price1 has a value of {price1} and is saved at {id(price1)}")
+print(f"the variable price2 has a value of {price2} and is saved at {id(price2)}")
