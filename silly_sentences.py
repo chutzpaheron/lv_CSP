6ac05b22cf9654927b1f673a61c30ab5feadca5c
chutzpaheron <laura.villarreal@ucas-edu.net>
2026-09-21 14:51:55 -0600

    silly_sentences

---
+++ silly_sentences.py
@@ -0,0 +1,30 @@
+# LV, period 7, silly sentences
+# it was a [] and blank [] night in [], and nothing could be heard throughout the house except for the scurries of a []. [] was
+
+adjective1 = input("say an adjective:").strip().capitalize()
+
+adjective2 = input("say another one!!:").strip().capitalize()
+
+place = input("now say a place (city, state, country):").strip().capitalize()
+
+animal1 = input("say the name of an animal:").strip().lower()
+
+person = input("type in the name of someone:").strip().capitalize()
+
+eat = input("type in anything edible:").strip().lower()
+
+liquid = input("now type in a liquid:").strip().lower()
+
+animalplural = input("now think of another animal. multiple animals. type it in. i mean like one species of animal.its plural like tigerS or dogS:").strip().lower()
+
+verbing = input("now say any verb ending in -ing:").strip().lower()
+
+emotion1 = input("think of an emotion any emotion. write it here:").strip().lower()
+
+emotion2 = input("NOW THINK OF ANOTHER:").strip().lower()
+
+localplace = input("type in another location:")
+
+adjective3 = input("one last one. write down a third adjective:")
+
+print("it was a very " + adjective1 + " " + "and " + adjective2 + " night in " + place + "," + " and nothing could be heard throughout the house except for the delicate calls of a graceful " + animal1 + ". " + person + " was hungry, and made some nice " + eat + " to go along with a cup of " + liquid + "." + " outside, there could be seen the silhouettes of " + animalplural + "," + " " + verbing + " in the silent forest. " + person + " saw this, and immediately felt an overwhelming sense of " + emotion1 + " going outside and " + verbing + " along with the animals. " + emotion2 + "with the beautiful animal encounter, " + person + " went back upstairs to " + localplace + ", and dozed off to bed. what a " + adjective3 + "night!")
