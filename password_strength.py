33d831fcd955accde48562c19224dd52764ec07a
chutzpaheron <laura.villarreal@ucas-edu.net>
2026-09-23 14:48:12 -0600

    password_strength

---
+++ password_strength.py
@@ -0,0 +1,51 @@
+# LV, period 7, password checker
+#im not sure if this is how you wanted it done but if it works it works
+
+charactercount = False
+upper = False
+lower = False
+numbers = False
+symbols = False
+strength = 0
+
+password = input("what is your password?:")
+
+if len(password) >= 8:
+        charactercount = True
+
+for letter in password:
+
+    if letter.isupper():
+        upper = True
+
+    if letter.islower():
+        lower = True
+
+    if letter.isnumeric():
+        numbers = True
+
+    if letter in "~!@#$%^&*()_+|:<>?-=[],./":
+        symbols = True
+
+if charactercount == True:
+    strength = strength + 1
+    print("your password has over eight characters. 1 point!")
+if upper == True:
+    strength = strength + 1
+    print("your password has at least one uppercase letter. 1 point!")
+if lower == True:
+    strength = strength + 1
+    print("your password has at least one lowercase letter. 1 point!")
+if numbers == True:
+    strength = strength + 1
+    print("your password has at least one number. 1 point!")
+if symbols == True:
+    strength = strength + 1
+    print("your password has at least one special character. 1 point!")
+
+if strength == 5:
+    print("you have a STRONG password!")
+elif strength > 2 and strength < 5:
+    print("you have a MEDIUM password!")
+else:
+    print("you have a WEAK password!")
